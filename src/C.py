d51eab99c50ed078c378382a9bb51bfdbd1f8f54
RS181 <ruisantos.18181@gmail.com>
2024-03-14 16:07:52 +0000

    alinea c completa (Verificar se estamos a medir tempo de forma correta)

--- src/C.py
+++ src/C.py
@@ -1,8 +1,11 @@
 # Codigo utilizado para alinea C (RSA)
+# IMPORTANTE: não consideramos para os tempos  a geração da chave privada
+# TODO verificar se tempo esta a ser medido de forma correta
 
 from cryptography.hazmat.primitives.asymmetric import rsa,padding
 from cryptography.hazmat.primitives import serialization,hashes
 import os
+import timeit
 
 #reads the contens of file an returns the string
 # or 0 if error ocurred 
@@ -53,24 +56,73 @@ def decrypt(private_key,ciphertext):
 
 
 
-def alinea_c(input_file):
+# receives input_file and n which is the number of times
+# we should measure encrypt and decrypt .Stores the results 
+# in output_result
+def alinea_c(input_file,n,output_result):
     text_to_cypher = read(input_file)
     if text_to_cypher is None:
         return
-    encrypted_message = encrypt(public_key,text_to_cypher)
-    print("Mensagem encriptada: ",encrypted_message)
-
-    # Decripting the message 
-    decrypted_message = decrypt(private_key,encrypted_message)
-    print("Mensagem desencriptada",decrypted_message)
     
+    #Arrays that hold encryption and decryption times measurements
+    encryption_measurements = []
+    decryption_measurements = []
+
+
+    #repeat n times 
+    for _ in range(n):
+        #measure the time for encryption
+        encryption_time = timeit.timeit(lambda:encrypt(public_key,text_to_cypher),number =1)
+        encryption_measurements.append(encryption_time)
+
+        #encrypt the text
+        ciphertext = encrypt(public_key,text_to_cypher)
+
+        #measure the time for decryption
+        decryption_time = timeit.timeit(lambda:decrypt(private_key,ciphertext),number=1)
+        decryption_measurements.append(decryption_time)
+
+        #decrypt the text 
+        plaintext = decrypt(private_key,ciphertext)
+
+    #save the result to output_result
+    save_data_to_file(output_result,encryption_measurements,decryption_measurements)
+
+
+#saves the measurments made to a file 
+def save_data_to_file (filename,encryption_times,decryption_times):
+    try: 
+        with open(filename,"w") as f:
+            f.write(f"Encryption times:{encryption_times}\n")
+            f.write(f"Decryption times:{decryption_times}\n")
+        print (f"Data saved successfuly to {filename}")
+    except Exception as e:
+        print(f"Error saving data to {filename}:{e}")
+
+
+
+#specify the file and number of times to measure encrypt and decrypt
+# Note: we consider that 100 measurements are statistically sufficient to have 
+# statistically significant results.   
+def do_test_for_RSA(): 
+    current_directory = os.getcwd()
+    i = 2
+    while (i <= 128):
+        # current test file name 
+        test_file = str(i) + ".txt"
+        #path tp text file for encription
+        file_path = os.path.join(current_directory,"test-files",test_file)
+        #path to text file to save results
+        output_result = os.path.join(current_directory,"out","RSA",test_file)
+
+        #do measurements and save the results 
+        alinea_c(file_path,100,output_result)
+        i *= 2
+
 
 #generate the pair of RSA keys
 private_key = generate_private_key()
 public_key = private_key.public_key()
 
-current_directory = os.getcwd()
-filepath = os.path.join(current_directory,"test-files","64.txt")
-print(filepath)
-alinea_c(filepath)
-alinea_c(filepath)
+
+do_test_for_RSA()
