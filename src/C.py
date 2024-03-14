d74aa9dc1b4311c0bbfb11e779f0dc10ff197da1
RS181 <ruisantos.18181@gmail.com>
2024-03-14 15:32:03 +0000

    adição de leitura de ficheiros

--- src/C.py
+++ src/C.py
@@ -2,6 +2,19 @@
 
 from cryptography.hazmat.primitives.asymmetric import rsa,padding
 from cryptography.hazmat.primitives import serialization,hashes
+import os
+
+#reads the contens of file an returns the string
+# or 0 if error ocurred 
+def read(input_file):
+    try:
+        with open(input_file,"rb") as f:
+            data = f.read()
+    except FileNotFoundError:
+        print("Ficheiro " + input_file + " não encontrado")
+        return None
+    return data
+
 
 #generate a private key 
 def generate_private_key():
@@ -40,19 +53,24 @@ def decrypt(private_key,ciphertext):
 
 
 
+def alinea_c(input_file):
+    text_to_cypher = read(input_file)
+    if text_to_cypher is None:
+        return
+    encrypted_message = encrypt(public_key,text_to_cypher)
+    print("Mensagem encriptada: ",encrypted_message)
+
+    # Decripting the message 
+    decrypted_message = decrypt(private_key,encrypted_message)
+    print("Mensagem desencriptada",decrypted_message)
+    
+
 #generate the pair of RSA keys
 private_key = generate_private_key()
 public_key = private_key.public_key()
 
-#message (the prefix b, indicates that the string is a sequence of bytes
-#instead of a sequence of characters. If we didn't do this we would get an error)
-message = b" mensagem aleatoria"
-
-# Encripting the message 
-encrypted_message = encrypt(public_key,message)
-print("Mensagem encriptada: ",encrypted_message)
-
-# Decripting the message 
-decrypted_message = decrypt(private_key,encrypted_message)
-print("Mensagem desencriptada",decrypted_message)
-
+current_directory = os.getcwd()
+filepath = os.path.join(current_directory,"test-files","64.txt")
+print(filepath)
+alinea_c(filepath)
+alinea_c(filepath)
